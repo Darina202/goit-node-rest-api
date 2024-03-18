4a717891884622eccaaa780bf04810e0b3627124
Darina202 <138057834+Darina202@users.noreply.github.com>
2024-03-18 20:05:01 +0200

    homework2

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,61 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from '../services/contactsServices.js';
+import HttpError from '../helpers/HttpError.js';
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const result = await contactsService.getContactById(req.params.id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const result = await contactsService.removeContact(req.params.id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  try {
+    const result = await contactsService.addContact(name, email, phone);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  const { params, body } = req;
+  try {
+    if (Object.keys(body).length === 0) {
+      throw HttpError(400, 'Body must have at least one field');
+    }
+    const result = await contactsService.updateContact(params.id, body);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
